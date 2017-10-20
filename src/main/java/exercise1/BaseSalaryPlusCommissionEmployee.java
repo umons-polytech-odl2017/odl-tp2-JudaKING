e14683fda0447f0890cd0d6149cc379781e8f25f
JudaKING <Julien.DALNE@student.umons.ac.be>
2017-10-20 15:50:02 +0200

    exo1-premiere partie

---
+++ src/main/java/exercise1/BaseSalaryPlusCommissionEmployee.java
@@ -0,0 +1,21 @@
+package exercise1;
+
+public class BaseSalaryPlusCommissionEmployee extends  Employee {
+
+
+	private int baseSalary;
+	private int commission;
+
+	public BaseSalaryPlusCommissionEmployee(String name, int baseSalary, int commission) {
+		super(name);
+		this.baseSalary = baseSalary;
+		this.commission=commission;
+	}
+
+
+	@Override
+	public int computeSalary() {
+
+		return (baseSalary+commission*sell);
+	}
+}
